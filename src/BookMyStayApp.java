cd318b084e5662ca11360bb1e69b542f8d7d0734
udayasimhareddy <udayasimhareddy@gmail.com>
2026-04-01 16:22:01 +0530

    UC 2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,7 +1,73 @@
-public class BookMyStayApp {
+abstract class Room {
+    int beds;
+    double price;
+    String type;
+
+    // Constructor
+    Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    // Method to display room details
+    void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price: " + price);
+    }
+}
+
+// Single Room
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+}
+
+// Double Room
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+}
+
+// Suite Room
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+}
+
+public class BookMyStayaApp {
     public static void main(String[] args) {
+
+        // UC1 (already done)
         System.out.println("=== Palindrome Checker Application ===");
         System.out.println("Version: 1.0");
-        System.out.println("Application Started Successfully");
+
+        // UC2 starts here
+
+        // Create room objects (Polymorphism)
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        // Static availability
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Display details
+        System.out.println("\n--- Room Details ---");
+
+        r1.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+
+        r2.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+
+        r3.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
