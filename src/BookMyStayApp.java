742d2067c905228e8545903acae55a1a2013b397
udayasimhareddy <udayasimhareddy@gmail.com>
2026-04-01 16:15:47 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,7 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("=== Palindrome Checker Application ===");
+        System.out.println("Version: 1.0");
+        System.out.println("Application Started Successfully");
+    }
+}
